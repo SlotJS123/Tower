2e2e8a0a47e6f3ee4d9d017376190a167ec3530f
YangWoongmo <yom3795@gmail.com>
2024-04-03 15:06:22 +0900

    Tile class 재설계

--- Assets/JS/Scripts/Tile.cs
+++ Assets/JS/Scripts/Tile.cs
@@ -1,20 +1,70 @@
 using System.Collections;
 using UnityEngine;
 using System;
+using UnityEngine.EventSystems;
 
-public enum TileState
+public enum TileType
 {
-    On,  //타워 설치가 가능한 상태
-    Off  //타워 설치가 불가능한 상태 
+    GroundTile,
+    WayTile
 }
 public class Tile : MonoBehaviour
 {
-    public TileState state;
-    protected bool canBuildTower;
-    public bool CanBuildTower => canBuildTower;
+    protected TileType tileType;
+    private EventTrigger clickTrigger;
+    private GameObject tower = null;
 
-    public void TouchTile()
+    private void Start()
     {
+        Init();
+    }
 
+    private void Init()
+    {
+        clickTrigger = GetComponent<EventTrigger>();
+
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+        entry.eventID = EventTriggerType.PointerClick;
+
+        entry.callback.AddListener((eventData) => { TouchTile(); });
+        clickTrigger.triggers.Add(entry);
+    }
+
+    private void TouchTile()
+    {
+        switch (tileType)
+        {
+            case TileType.GroundTile:
+                Debug.Log("click GroundTile");
+                CheckCanBuildTower();
+                return;
+
+            case TileType.WayTile:
+                Debug.Log("click WayTile");
+                return;
+        }
+    }
+
+    private void CheckCanBuildTower()
+    {
+        // 타워 설치가 가능할 때
+        if (tower == null)
+        {
+            if (!GameManager.Instance.IsCreateTower())
+            {
+                Debug.Log("설치 가능한 타워가 선택되지 않았습니다");
+                return;
+            }
+
+            tower = GameManager.Instance.InstantiateTower();
+            tower.transform.position = this.transform.position;
+            return;
+        }
+
+        // 타워가 이미 설치되어 있을 때
+        // 현재는 임시로 타워 바로 제거
+        Debug.Log("타워 제거");
+        Destroy(tower.gameObject);
+        tower = null;
     }
 }
