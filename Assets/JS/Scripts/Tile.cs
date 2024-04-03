fd7068163781db50c241b863ad878690f797591a
YangWoongmo <yom3795@gmail.com>
2024-04-03 12:50:58 +0900

    Tile Class의 길 찾기 관련 코드 제거

--- Assets/JS/Scripts/Tile.cs
+++ Assets/JS/Scripts/Tile.cs
@@ -1,10 +1,6 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using System.IO;
 using System;
-using UnityEngine.UIElements;
 
 public enum TileState
 {
@@ -13,110 +9,12 @@ public enum TileState
 }
 public class Tile : MonoBehaviour
 {
-    public UnityEngine.UI.Button touchButton;
-
-    public Tile(bool _isWall, int _x, int _y) { isWall = _isWall; x = _x; y = _y; }
-
-    public bool isWall;
-    public Tile ParentNode;
-
-    public GameObject tile_1;
-    public GameObject tile_2;
-
-
-    // G : 시작으로부터 이동했던 거리, H : |가로|+|세로| 장애물 무시하여 목표까지의 거리, F : G + H
-    public int x, y, G, H;
-    public int F { get { return G + H; } }
     public TileState state;
+    protected bool canBuildTower;
+    public bool CanBuildTower => canBuildTower;
 
-    public Action<Tile> OnTileClick;
-
-    // public TileState TileState => state;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        if(touchButton != null)
-        touchButton.onClick.AddListener(TouchTile);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    /// <summary>
-    ///  Map Manager에서 map을 만들 때 tile 프리팹을 생성하며 가장 먼저 실행하기 될 함수입니다 
-    /// </summary>
-    /// <param name="_x">x 좌표값을 할당 받을지 몰라서 준비 </param>
-    /// <param name="_y">y 좌표값을 할당 받을지 몰라서 준비 </param>
-    public void Info(int _x, int _y)
-    {
-        x = _x; y = _y; 
-        //일단 테스트용도로 On/Off로만 존재합니다 
-        // On인경우 몬스터의 이동경로가 되며
-        // Off인 경우 타워설치 경로가 됩니다 
-        state = TileState.On;
-        H = 190;
-        G = 10;
-        isWall = true;
-        tile_2.gameObject.SetActive(false);
-    }
-
-    /// <summary>
-    /// 버튼을 클릭했을 경우 실행되는 이벤트 함수입니다 
-    /// </summary>
     public void TouchTile()
     {
-        if (GameManager.Instance.mapManager.towerSetState == false)
-        {
-
-            state = TileState.Off;
-            isWall = false;
-
-            tile_1.gameObject.SetActive(false);
-            tile_2.gameObject.SetActive(true);
-
-            OnTileClick?.Invoke(this);
-        }
-        else
-        {
-            if (GameManager.Instance.selectTile != null)
-                return;
-
-            if (state == TileState.Off)
-                return;
-
-            Debug.Log("여기서 이제 타워를 생성해주는 로직을 생성해서 적용을 시켜야합니다 ");
-            GameManager.Instance.selectTile = this;
-            UIManager.Instance.OnClickEmptyZone();
-        }
-
-
-
-    }
-
-    public void TargetTile()
-    {
-        state = TileState.Off;
-        //OnTileClick?.Invoke(this);
-        isWall = false;
-
-        tile_1.gameObject.SetActive(false);
-        tile_2.gameObject.SetActive(true);
-    }
-
-
-
-
-    public void ResetData()
-    {
-        isWall = true;
-
-        state = TileState.On;
-        tile_1.gameObject.SetActive(true);
-        tile_2.gameObject.SetActive(false);
 
     }
 }
