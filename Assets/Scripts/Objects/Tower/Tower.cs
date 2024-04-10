7a288490d00e0b79c13fe37e11a9ead06eb0c7c9
YangWoongmo <yom3795@gmail.com>
2024-04-10 15:40:46 +0900

    미사용 using문 정리

--- Assets/Scripts/Objects/Tower/Tower.cs
+++ Assets/Scripts/Objects/Tower/Tower.cs
@@ -1,9 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
 public enum WeaponState { SearchTarget = 0, AttToTarget }
 
