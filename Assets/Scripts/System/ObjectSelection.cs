2057f91e445ce412e65249106b5dcb0ce9953c49
YangWoongmo <yom3795@gmail.com>
2024-04-10 16:07:42 +0900

    기존에 Raycast로 오브젝트 구분하던 Class 이름 변경 및 인터페이스를 구분하게 변경

--- Assets/Scripts/System/ObjectSelection.cs
+++ Assets/Scripts/System/ObjectSelection.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 
 
 
-public class SpriteSelection : MonoBehaviour
+public class ObjectSelection : MonoBehaviour
 {
-    /*
     private Camera mainCamera;
 
     void Start()
@@ -34,41 +33,22 @@ public class SpriteSelection : MonoBehaviour
             // 카메라에서 마우스 클릭 지점을 향하는 레이 생성
             Ray ray = new Ray(cameraPosition, mousePositionInWorld - cameraPosition);
             RaycastHit hit;
-            //Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-            //RaycastHit hit;
 
             // 레이캐스트로 오브젝트 충돌 확인
             if (Physics.Raycast(ray, out hit))
             {
-                // 충돌한 오브젝트가 스프라이트인지 확인
-                Tile spriteRenderer = hit.collider.GetComponent<Tile>();
+                IInteractionable interactionObject = hit.transform.GetComponent<IInteractionable>();
 
-                if(spriteRenderer == null)
+                if (interactionObject != null)
                 {
-                    return;
-
-                }
-
-
-                if (spriteRenderer.isWall == false && GameManager.Instance.mapManager.towerSetState == true)
-                {
-                    return;
-                }
-
-                if (spriteRenderer != null)
-                {
-                    // 스프라이트 오브젝트를 클릭한 경우
-                    Debug.Log("Sprite Object Selected: " + hit.collider.gameObject.name);
-
-                    spriteRenderer.TouchTile();
-                }
-                else
-                {
-                    // 스프라이트가 아닌 다른 오브젝트를 클릭한 경우
-                    Debug.Log("Other Object Selected: " + hit.collider.gameObject.name);
+                    CheckInteractableObject(interactionObject);
                 }
             }
         }
     }
-    */
+
+    private void CheckInteractableObject(IInteractionable interactionableObject)
+    {
+        interactionableObject.InteractionObject();
+    }
 }
