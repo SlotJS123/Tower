8913a5d59767f409a332f4af2b541c07f629b958
YangWoongmo <yom3795@gmail.com>
2024-04-10 16:05:42 +0900

    상호작용 가능한 오브젝트에 부착할 인터페이스 생성

---
+++ Assets/Scripts/System/IInteractionable.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 터치해야하는 오브젝트에 상속시킬 인터페이스
+public interface IInteractionable
+{
+    public void InteractionObject();
+}
