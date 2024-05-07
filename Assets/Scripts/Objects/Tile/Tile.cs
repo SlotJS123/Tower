6356b6fe9b2c9284c9d8ec56f8f6b793d8322b58
SlotJS123 <tlswnstjr7956@gmail.com>
2024-05-07 20:09:22 +0900

    0507-3

--- Assets/Scripts/Objects/Tile/Tile.cs
+++ Assets/Scripts/Objects/Tile/Tile.cs
@@ -29,6 +29,18 @@ public class Tile : MonoBehaviour, IInteractionable
         {
             case TileType.GroundTile:
                 Debug.Log("click GroundTile");
+                this.gameObject.SetActive(false);
+                GameManager.Instance.TowerManager.JS_TowerInstallation(this);
+                //다음 타워 설치에 대한 값을 실행여부를 확인하기 위한 이벤트입니다 
+                MapManager mapManager = GameManager.Instance.MapManager;
+
+                if (mapManager.TowerInstallCounter != 5)
+                {
+                    mapManager.TowerInstallCounter = mapManager.TowerInstallCounter + 1;
+                    mapManager.OnNextTower.Invoke();
+                }
+
+
                 return;
 
             case TileType.WayTile:
